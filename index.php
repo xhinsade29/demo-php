33f2bb2ee9e465e1dc9aef9cef292e66214bc7f3
xhinsade29 <lopezkristine479@gmail.com>
2025-03-05 19:49:45 +0800

    added comment for what is echo

--- index.php
+++ index.php
@@ -3,12 +3,17 @@
 echo "Hi, My name is Kristine L. Lopez. <br>"; 
 echo "I am a student from Northern Bukidnon State College. <br>";
 echo "I am taking up Bachelor of Science in Information Technology. <br>";
-echo "I am currently studying Web Development. <br>";
 echo "I am a 2nd year student.<br>";
 echo "Im interested in learning php and adding database to my website. <br>";
 echo "<br>";
 echo "Currently, i feel very overwhelmed with the things that i need to learn. <br>";
 echo "Im a bit tired and sleepy most of the time, I hardly to have enough focus on my studies. <br>";
 echo "i hope ill be to overcome this feeling soon. <br>";
+
+
+//echo is used to output data to the screen
+//It it similar to print, but echo can take multiple parameters directly to the web browser without using DOM.
+//echo has no return value
+
 
 ?>
