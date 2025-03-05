e9db17fe46001cc401cdc3db330bfa936de763ba
xhinsade29 <lopezkristine479@gmail.com>
2025-03-05 20:54:24 +0800

    added comments and explaination of what is get and how it works

--- get.php
+++ get.php
@@ -28,4 +28,33 @@ echo"<br>";
 echo "Name: $name <br>";
 echo "$name Feedback: $feedback";
 
+
+
+// Key points about GET method:
+// 1. Collects form data and sends to server
+// 2. Data is visible in URL
+// 3. Has size limitations
+// 4. Best for non-sensitive data
+// 5. Allows browser caching and bookmarking
+// 6. Not secure/encrypted
+
+
+/*Output Explaination
+ If i submit the name : Chichi and feedback: "I love the website"
+
+ The output would be Name: Chichi
+ Chichi Feedback: I love the website. 
+
+It uses get method to collect the data from the form and sends it to the server.
+The data is visible in the URL. Which is name=Chichi&feedback=I+love+the+website.
+It it unsafe to use get method for sensitive data because it is not secure/encrypted and it is visible in the URL. 
+
+
+We use $_GET to get the data from the form. 
+$_GET is a superglobal variable that is used to collect form data after submitting an HTML form with method="get".
+
+By submitting the form, the data is sent to the server and the server will process the data and display the output to the screen.
+We use echo to output the data to the screen.
+*/
+
 ?>
