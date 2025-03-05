5b224b0230b86379e9250f3ade8096d47cd41aed
xhinsade29 <lopezkristine479@gmail.com>
2025-03-06 07:25:15 +0800

    added explaination for handling password and username

--- post.php
+++ post.php
@@ -27,4 +27,21 @@ $password = $_POST['password'];
 echo "Name: $name <br>";
 echo "Password: $password";
 
+// Key points about POST method:
+// 1. Collects form data and sends to server
+// 2. Data is not visible in URL
+// 3. No size limitations
+// 4. Best for sensitive data
+// 5. Does not allow browser caching and bookmarking
+// 6. Secure/encrypted
+
+// Output Explaination
+// If i submit the name : Chichi and password: "1234"
+// The output would be Name: Chichi
+// Password: 1234
+
+// It uses post method to collect the data from the form and sends it to the server.
+// It is more secure than get method because the data is not visible in the URL.
+
+
 ?>
