a484f144f7bca535116e884c952e2735a2428c62
xhinsade29 <lopezkristine479@gmail.com>
2025-03-07 09:40:35 +0800

    added explaintion and comments

--- conditions.php
+++ conditions.php
@@ -41,16 +41,24 @@
             $isDiscount = false;
             $isExpensive = false;
 
+            // Using && (AND) operator: Both conditions must be true
+            // Checks if number is between 1 AND 5 inclusive
             if($number >= 1 && $number <= 5) {
+                
+                // Using || (OR) operator: At least one condition must be true
+                // Checks if item is number 4 OR number 2 (expensive items)
                 if($number == 4 || $number == 2) {
                     $isExpensive = true;
                     echo "<p style='color: red'>This is an expensive item!</p>";
                 }
 
+                // Using ! (NOT) operator: Reverses a boolean value
+                // Shows message if item is NOT expensive
                 if(!$isExpensive) {
                     echo "<p style='color: green'>This is an affordable item!</p>";
                 }
 
+                // Select item based on number input
                 if($number == 1){
                     echo "Item $number: $item1 costs $${item1_price}<br>";
                     $total = $item1_price;
@@ -76,7 +84,33 @@
                 echo "Please enter a number between 1 and 5";
             }
         }
+
+/*
+
+In this example, we have a list of items with corresponding prices. 
+The user can select an item by entering a number from 1 to 5. 
+The selected item and its price will be displayed along with a message indicating if it is an expensive or affordable item.
+
+we have a form that takes a number input from the user. 
+When the form is submitted, the PHP code checks if the number is within the range of 1 to 5.
+
+If the number is within the range, the code then checks if the selected item is an expensive item (item 2 or item 4).
+If the selected item is expensive, a message is displayed in red indicating that it is an expensive item.   
+If the selected item is not expensive, a message is displayed in green indicating that it is an affordable item.
+
+The code then displays the selected item and its price based on the user input.
+The total cost of the selected item is calculated and displayed.
+
+If the number is not within the range of 1 to 5, a message is displayed asking the user to enter a number between 1 and 5.
+
+We used logical operators such as && (AND), || (OR), and ! (NOT) to combine conditions and control the flow of the program based on the user input.
+We also used conditional statements (if, else if) to select the item and calculate the total cost.
+WE used isset() function to check if the form has been submitted and the number input is set.
+WE used $_POST to get the number input from the form using the POST method.
+*/
+
         ?>
     </div>
 </body>
 </html>
+
